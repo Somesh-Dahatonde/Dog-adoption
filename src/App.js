995fad811b4659fd5001e890a0ca4128e282e27a
somesh-dahatonde <sddahatonde22@gmail.com>
2022-04-30 23:51:41 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import { render } from "react-dom";
+import SearchParams from "./SearchParams";
 import { StrictMode } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Details from "./Details";
 
-// import Pet from "./Pet";
-import SearchParams from "./SearchParams";
 const App = () => {
   return (
     <StrictMode>
-      <div>
-        <BrowserRouter>
-          <h1>Adopt Me!</h1>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
-        </BrowserRouter>
-        ;
-      </div>
+      <BrowserRouter>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
     </StrictMode>
   );
 };
+
 render(<App />, document.getElementById("root"));
